155f1846c2038fa10c34125124f644994c8f5caf
aissatou00 <149043243+aissatou00@users.noreply.github.com>
2026-03-13 12:21:39 +0100

    Fix formatting of type check in TemplateDealsAdapter

--- backend/app/adapter/TemplateDealsAdapter.py
+++ backend/app/adapter/TemplateDealsAdapter.py
@@ -20,7 +20,7 @@ class TemplateDealsAdapter:
         value = obj
 
         for key in keys:
-            if type(value)==dict:
+            if type(value) == dict:
                 value = value.get(key)
             else:
                 return None
